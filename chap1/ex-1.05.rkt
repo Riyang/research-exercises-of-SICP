ca6557a72fe8d371d5d03b21fa4bf58e1eb82ac7
Riyang <1285608014@qq.com>
2017-12-29 09:56:25 +0800

    Update ex-1.05.rkt

--- chap1/ex-1.05.rkt
+++ chap1/ex-1.05.rkt
@@ -11,7 +11,8 @@
 ; ---- Analysis ----
 ; What's the value of (test 0 (p))?
 ; There are two models(orders) of evaluation:
-; Applicative Order and Normal Order
+; Applicative Order and Normal Order.
+;
 ; In applicative order:
 ; the interpreter will first evaluates test and 0, (p),
 ; and then applies test to 0 and (p).
@@ -20,20 +21,35 @@
 ; What's 0 and (p)?
 ; Well, 0 is 0 itself. But what is (p)?
 ; We can also see the definition of (p). What is it?
-; What is (define (p) (p))? Did it tell us what the (p) is?
-; No. (p) is nothing we can know.
+; What does (define (p) (p)) mean? Did it tell us what the (p) is?
+; No. It told us that (p) is (p) itself.
+; It's just like that:
+; I ask, "what's your name?"
+; You answer, "My name is my name."
+; So, it's bullshit.
+; (p) is nothing we can know.
 ; Thus, if the interpreter evaluates the operand (p),
 ; it will do an endless evaluation.
 ; So, if your interpreter to evaluate (test 0 (p)),
-; you will get nothing, just make the interpreter do an endless work.
+; you will get nothing, but just make the interpreter do an endless work.
 ;
 ; In normal order:
-; The interpreter will not evaluate the operands
+; The interpreter will not evaluate the operands,
 ; until their values were needed.
 ; So, when it evaluate (test 0 (p)), it will not evaluate the values
 ; of 0 and (p), until it needs it.
 ; (test 0 (p)) will just applies test to 0 and (p).
-; it will come to the if expression of test's definition.
+; It will come to the if expression of test's definition:
 ; (if (= x 0) 0 y)
-; Here, x is 0,
-; so, (test 0 (p)) is 0.
+; What is x? x is the first argument of test.
+; So, what is it? Of course, it is 0.
+; And then?
+; (if (= x 0) 0 y) is 0.
+; Thus, (test 0 (p)) is 0.
+;
+; In general:
+; When you evaluate (test 0 (p)) on your interpreter,
+; if you get nothing, it means your interpreter uses applicative-order evaluation;
+; if you get 0, it means your interpreter uses normal-order evaluation.
+;
+; That's it.
