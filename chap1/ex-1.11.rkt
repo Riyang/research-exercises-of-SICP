961aec53915fc483fc8d3cc835841753eaa92432
Riyang <1285608014@qq.com>
2017-12-29 21:47:28 +0800

    Update ex-1.11.rkt

--- chap1/ex-1.11.rkt
+++ chap1/ex-1.11.rkt
@@ -1,13 +1,20 @@
-;; f(n)=n, if n < 3; 
-;; f(n) = f(n-1) + 2f(n-2) + 3f(n-3), if n >= 3.
-;; recursive process:
+; Exercise 1.11
+
+; f(n)=n, if n < 3; 
+; f(n) = f(n-1) + 2f(n-2) + 3f(n-3), if n >= 3.
+
+; recursive process definition:
 (define (f n)
   (cond ((< n 3) n)
         (else (+ (f (- n 1))
                  (* 2 (f (- n 2)))
                  (* 3 (f (- n 3)))))))
-;; iterative process:
-(define (f n) (f-iter 2 1 0 n))
+
+; iterative process definition:
+(define (f n)
+  (if (< n 3)
+      n
+      (f-iter 2 1 0 n)))
 (define (f-iter a b c count)
   (if (= count 0)
       c
@@ -17,3 +24,13 @@
               a
               b
               (- count 1))))
+
+; test:
+(f -3)    ; => -3
+(f -2)    ; => -2
+(f -1)    ; => -1
+(f 0)     ; => 0
+(f 1)     ; => 1
+(f 2)     ; => 2
+(f 3)     ; => 4
+(f 4)     ; => 11
