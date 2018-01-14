098d28022a17aaa5cb8ba3879a91cb96c47b9770
R. Yang Hwang <1285608014@qq.com>
2018-01-14 21:40:34 +0800

    Create ex-1.07.rkt

---
+++ chap1/ex-1.07.rkt
@@ -0,0 +1,30 @@
+; Exercise 1.7
+
+; predefine:
+(define (Ysqrt x) (sqrt-iter 1.0 x))
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x) x)))
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+(define (improve guess x)
+  (average guess (/ x guess)))
+(define (average a b)
+  (/ (+ a b) 2.0))
+(define (square x) (* x x))
+
+; Part 1: try some small numbers, and some large numbers
+; small numbers:
+(Ysqrt 0.01)
+(Ysqrt 0.0001)
+(Ysqrt 0.000001)
+(Ysqrt 0.00000001)
+(Ysqrt 0.0000000001)
+(Ysqrt 0.0000000000000001)
+
+; large numbers:
+
+; Part 2: change the good-enough procedure to a better method
+
+; To be continue ...
